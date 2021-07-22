dd3daab258eab349940c61665ba9a221c38fa497
sp010412 <smajwede7@gmail.com>
2021-07-22 18:27:52 +0200

    complete

--- greet.js
+++ greet.js
@@ -1,9 +1,3 @@
-// function greet(name){
-//     return 'Hello, ' + name;
-//   }
-
-//   var msg = greet('Xola')
-//   console.log(msg)
 module.exports = function(name){
     return 'Hello, ' + name;
 }
